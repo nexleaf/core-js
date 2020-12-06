6e1c8b21cc532354c2fd190269c45ce6c1066c68
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-06 22:00:36 +0700

    drop `esnext.math.seeded-prng`

--- tests/commonjs.js
+++ tests/commonjs.js
@@ -136,7 +136,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
   ok(load('features/math/radians')(180) === Math.PI);
   ok(load('features/math/scale')(3, 1, 2, 1, 2) === 3);
   ok(load('features/math/signbit')(-2) === true);
-  ok(typeof load('features/math/seeded-prng')({ seed: 42 }).next().value === 'number');
   ok(load('features/number/constructor')('5') === 5);
   ok(load('features/number/epsilon') === 2 ** -52);
   ok(load('features/number/is-finite')(42.5));
@@ -924,7 +923,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
   load('proposals/reflect-metadata');
   load('proposals/relative-indexing-method');
   load('proposals/keys-composition');
-  load('proposals/seeded-random');
   load('proposals/set-methods');
   load('proposals/string-code-points');
   load('proposals/using-statement');
